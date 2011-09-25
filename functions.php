6fb2cc77f3abb4495964bc60445ea3d149f764a8
Rich Berrill <rberrill@gmail.com>
2011-09-24 20:44:21 -0500

    Modified link colors to try to better fit the theme.

--- functions.php
+++ functions.php
@@ -84,3 +84,11 @@ function removeHeadLink() {
     remove_action('wp_head', 'rsd_link');
     remove_action('wp_head', 'wlwmanifest_link');
 }
+
+// add ie conditional html5 shim to header
+function add_ie_html5_shim () {
+    echo '<!--[if lt IE 9]>';
+    echo '<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>';
+    echo '<![endif]-->';
+}
+add_action('wp_head', 'add_ie_html5_shim');
